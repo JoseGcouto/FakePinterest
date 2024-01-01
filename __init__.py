cfcb5d5ba7a55e8aec3eef8e8d3dbdc660de96dc
José Augusto <contatojose.gcouto@gmail.com>
2024-01-01 19:33:18 -0300

    deploy atualização

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = 'a277d94ed2cc0ff474d897116c540777'
+app.config["UPLOAD_FOLDER"] = 'static/fotos_posts'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
